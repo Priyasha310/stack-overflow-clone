f6bca70791ff6f2033290fa008dc187f9aa44ae1
Priyasha310 <72645022+Priyasha310@users.noreply.github.com>
2023-10-29 13:48:04 +0530

    navigation & click conflict resolve

--- frontend/src/RoutesComponent.js
+++ frontend/src/RoutesComponent.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Auth from './pages/Auth/Auth'
-import WidgetTags from 'components/RightSideBar/WidgetTags'
 import Questions from 'pages/Questions/Questions'
+import AskQuestions from 'pages/AskQuestions/AskQuestions'
+import DisplayQuestion from 'pages/Questions/DisplayQuestion'
+import Tags from 'pages/Tags/Tags'
 
 const RoutesComponent = () => {
 
   return (
       <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/Auth' element={<Auth/>} />
-          <Route path='/Questions' element={<Questions/>} />
-          <Route path='/Tags' element={<WidgetTags/>} />
-          {/* <Route path='/Users' element={<Users/>} /> */}
+          <Route path='/auth' element={<Auth/>} />
+          <Route path='/questions' element={<Questions/>} />
+          <Route path='/tags' element={<Tags/>} />
+          <Route path='/questions/:id' element={<DisplayQuestion/>} />
+          <Route path='/questions/ask' element={<AskQuestions/>} />
       </Routes>
   )
 }
--- frontend/src/components/LeftSideBar/LeftSideBar.jsx
+++ frontend/src/components/LeftSideBar/LeftSideBar.jsx
@@ -5,47 +5,45 @@ import globe from '../../assets/Globe.svg'
 
 const LeftSideBar = () => {
 
-  const [isActive, setIsActive] = useState('Home');
-
-  const setTabActive = (tabName) => {
-    setIsActive(tabName);
-  };
-
 
   return (
     <div className={styles.leftSidebar}>
       <nav className={styles.sideNav}>
-        <NavLink to='/'  className={`${styles.sideNavLinks} ${isActive === 'Home'?styles.active:''}`}  onClick={()=>setIsActive('Home')}>        
+        <NavLink to='/' className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>        
         <p>Home</p></NavLink>
         
         <div className={styles.sideNavDiv}>
           <div className={styles.divHeading}><p>Public</p></div>
-            <NavLink to='/Questions' className={`${styles.sideNavLinks} ${isActive === 'Questions'?styles.active:''}`} onClick={()=>setIsActive('Questions')}>
+            <NavLink to='/questions' className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>
               <img src={globe} alt='Globe'/>
               <p>Questions</p>
             </NavLink>
-
-            <NavLink to='/Tags' className={`${styles.sideNavLinks} ${isActive === 'Tags'?styles.active:''}`} 
-            onClick={()=>setIsActive('Tags')}><p className='pl-5'>Tags</p></NavLink>
             
-            <NavLink to='/Users' className={`${styles.sideNavLinks} ${isActive === 'Users'?styles.active:''}`} onClick={()=>setIsActive('Users')}>            
-            <p className='pl-5'>Users</p></NavLink>
+            <NavLink to='/tags' className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>
+              <p className='pl-5'>Tags</p>
+            </NavLink>
             
-            <NavLink to='/' className={`${styles.sideNavLinks} ${isActive === 'Companies'?styles.active:''}`} onClick={()=>setIsActive('Companies')}>            
-            <p className='pl-5'>Companies</p></NavLink>
+            <NavLink to='/users' className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>            
+              <p className='pl-5'>Users</p>
+            </NavLink>
+            
+            <NavLink to='/companies' className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>            
+              <p className='pl-5'>Companies</p>
+            </NavLink>
         </div>
 
         <div className={styles.sideNavDiv}>
           <div className={styles.divHeading}>Collectives</div>
-          
-          <NavLink to='/' className={`${styles.sideNavLinks} ${isActive === 'Explore'?styles.active:''}`} onClick={()=>setIsActive('Explore')}>          
-          <p className='pl-5'>Explore Collectives</p></NavLink>
+          <NavLink to='/explore-collectives'  className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>          
+            <p className='pl-5'>Explore Collectives</p>
+          </NavLink>
         </div>
+        
         <div className={styles.sideNavDiv}>
           <div className={styles.divHeading}>Labs</div>
-          
-          <NavLink to='/' className={`${styles.sideNavLinks} ${isActive === 'Discussions'?styles.active:''}`} onClick={()=>setIsActive('Discussions')}>          
-          <p className='pl-5'>Discussions</p></NavLink>
+          <NavLink to='/discussions'  className={(props)=>`${props.isActive? styles.active: ''} ${styles.sideNavLinks}`}>          
+            <p className='pl-5'>Discussions</p>
+          </NavLink>
         </div>
       </nav>
     </div>
